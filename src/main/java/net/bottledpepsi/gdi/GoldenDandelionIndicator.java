00d1454ae21646a11960d1b05f94644e026967e8
bottledpepsi <ree.ruben@proton.me>
2026-03-31 17:16:02 +1100

    NeoForge branch

--- src/main/java/net/bottledpepsi/gdi/GoldenDandelionIndicator.java
+++ src/main/java/net/bottledpepsi/gdi/GoldenDandelionIndicator.java
@@ -1,15 +1,8 @@
 package net.bottledpepsi.gdi;
 
-import net.fabricmc.api.ModInitializer;
+import net.neoforged.fml.common.Mod;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public class GoldenDandelionIndicator implements ModInitializer {
-	public static final String MOD_ID = "goldendandelionindicator";
-	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
-
-	@Override
-	public void onInitialize() {
-	}
+@Mod(GoldenDandelionIndicator.MODID)
+public class GoldenDandelionIndicator {
+    public static final String MODID = "goldendandelionindicator";
 }
---
+++ src/main/java/net/bottledpepsi/gdi/GoldenDandelionIndicatorClient.java
@@ -0,0 +1,12 @@
+package net.bottledpepsi.gdi;
+
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+
+
+public class GoldenDandelionIndicatorClient {
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+    }
+}
